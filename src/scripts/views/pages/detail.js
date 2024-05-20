e1ba25bf2d1b107217f7cae223d518aaf80dcb25
Miftah Fauzy <136819861+Miftah130300@users.noreply.github.com>
2024-05-20 16:13:05 +0700

    Add files via upload

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -3,35 +3,20 @@
 import UrlParser from '../../routes/url-parser'
 import TheRestoSource from '../../data/theresto-source'
 import { createRestoDetailTemplate } from '../templates/template-creator'
-import LikeButtonPresenter from '../../utils/like-button-presenter'
-import FavoriteRestoIdb from '../../data/favorite-resto-idb'
 
 const Detail = {
   async render () {
     return `
         <div class="detailResto" id="detailResto"></div>
-        <div id="likeButtonContainer"></div>
     `
   },
 
   async afterRender () {
     const url = UrlParser.parseActiveUrlWithoutCombiner()
-    const restaurant = await TheRestoSource.detailRestaurant(url.id)
+    const restaurant = await TheRestoSource.detailResto(url.id)
     const restoContainer = document.querySelector('#detailResto')
     restoContainer.innerHTML = createRestoDetailTemplate(restaurant)
     console.log(restaurant)
-    LikeButtonPresenter.init({
-      likeButtonContainer: document.querySelector('#likeButtonContainer'),
-      favoriteRestos: FavoriteRestoIdb,
-      restaurant: {
-        id: restaurant.id,
-        name: restaurant.name,
-        city: restaurant.city,
-        description: restaurant.description,
-        pictureId: restaurant.pictureId,
-        rating: restaurant.rating
-      }
-    })
   }
 }
 
--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default
-const ImageminMozjpeg = require('imagemin-mozjpeg')
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 const path = require('path')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
   entry: {
@@ -14,29 +10,6 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      minSize: 20000,
-      maxSize: 70000,
-      minChunks: 1,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      automaticNameDelimiter: '~',
-      enforceSizeThreshold: 50000,
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true
-        }
-      }
-    }
   },
   module: {
     rules: [
@@ -54,42 +27,10 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/public'),
-          to: path.resolve(__dirname, 'dist'),
-          globOptions: {
-          // CopyWebpackPlugin mengabaikan berkas yang berada di dalam folder images
-            ignore: ['**/images/**']
-          }
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/')
         }
       ]
-    }),
-    new WorkboxWebpackPlugin.GenerateSW({
-      swDest: './sw.bundle.js',
-      runtimeCaching: [
-        {
-          urlPattern: /^https:\/\/restaurant-api.dicoding.dev\//,
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'theresto-api'
-          }
-        },
-        {
-          urlPattern: /^https:\/\/restaurant-api.dicoding.dev\/images\/medium\//,
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'theresto-image-api'
-          }
-        }
-      ]
-    }),
-    new ImageminWebpackPlugin({
-      plugins: [
-        ImageminMozjpeg({
-          quality: 50,
-          progressive: true
-        })
-      ]
-    }),
-    new BundleAnalyzerPlugin()
+    })
   ]
 }
